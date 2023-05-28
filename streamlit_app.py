0058c857538cfa0b951a912f1a08fa898f534a8a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-05-28 15:23:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -222,3 +222,4 @@ else:
     if st.button("Next Step"):
         with st.spinner("Thinking"):
             st.session_state.short_memory, st.session_state.long_memory, st.session_state.written_paras, st.session_state.instruction1, st.session_state.instruction2, st.session_state.instruction3 = controled_step(st.session_state.short_memory, st.session_state.long_memory, selected_instruction, st.session_state.written_paras)
+            st.experimental_rerun()
